5b98b170689df2e847347075b24bf0d03c4ae13c
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-09-12 15:57:33 +0200

    Update shell.nix to conditionally add linux clipboard accessor packages

--- shell.nix
+++ shell.nix
@@ -6,5 +6,11 @@ pkgs.mkShell {
     pkgs.python3Packages.pillow
     pkgs.python3Packages.numpy
     pkgs.python3Packages.scipy
-  ];
+  ] ++ (if builtins.getEnv "XDG_SESSION_TYPE" == "x11" then
+          [ pkgs.xclip ]
+        else if builtins.getEnv "XDG_SESSION_TYPE" == "wayland" then
+          [ pkgs.wl-clipboard ]
+        else
+          []
+       );
 }
