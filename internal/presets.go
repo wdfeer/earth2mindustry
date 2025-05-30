6eef57d8f8e9040dd8ef4946a1f4db7fd823eb73
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-05-30 22:08:55 +0200

    Create snow, desert

--- internal/presets.go
+++ internal/presets.go
@@ -32,21 +32,31 @@ var Presets map[string]Preset = map[string]Preset{}
 func Initialize_presets() {
 	Presets["default"] =
 		map[px.Pixel]px.Pixel{
-			{R: 132, G: 215, B: 235}: tiles.Deep_water,
-			{R: 207, G: 246, B: 224}: tiles.Grass,
-			{R: 154, G: 229, B: 194}: tiles.Grass,
-			{R: 245, G: 240, B: 230}: tiles.Sand,
-			{R: 247, G: 247, B: 247}: tiles.Snow,
-			{R: 167, G: 187, B: 214}: tiles.Stone,
-		}
-
-	Presets["urban"] =
-		map[px.Pixel]px.Pixel{
 			{R: 132, G: 215, B: 235}: tiles.Shallow_water,
 			{R: 207, G: 246, B: 224}: tiles.Grass,
 			{R: 154, G: 229, B: 194}: tiles.Grass,
 			{R: 245, G: 240, B: 230}: tiles.Sand,
 			{R: 247, G: 247, B: 247}: tiles.Stone,
+			{R: 167, G: 187, B: 214}: tiles.Dark_sand,
+		}
+
+	Presets["snow"] =
+		map[px.Pixel]px.Pixel{
+			{R: 132, G: 215, B: 235}: tiles.Deep_water,
+			{R: 207, G: 246, B: 224}: tiles.Snow,
+			{R: 154, G: 229, B: 194}: tiles.Grass,
+			{R: 245, G: 240, B: 230}: tiles.Sand,
+			{R: 247, G: 247, B: 247}: tiles.Snow,
+			{R: 167, G: 187, B: 214}: tiles.Dark_sand,
+		}
+
+	Presets["desert"] =
+		map[px.Pixel]px.Pixel{
+			{R: 132, G: 215, B: 235}: tiles.Deep_water,
+			{R: 207, G: 246, B: 224}: tiles.Snow,
+			{R: 154, G: 229, B: 194}: tiles.Grass,
+			{R: 245, G: 240, B: 230}: tiles.Sand,
+			{R: 247, G: 247, B: 247}: tiles.Sand,
 			{R: 167, G: 187, B: 214}: tiles.Dark_sand,
 		}
 
