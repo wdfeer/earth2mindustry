d6d25436b46424886b275dc4cf783d14ff1f6b5d
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-05-30 10:10:41 +0200

    Implement reading preset name as cli arg

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module earth2mindustry
 
 go 1.24.3
+
+require github.com/samber/mo v1.14.0
--- internal/presets.go
+++ internal/presets.go
@@ -1,13 +1,31 @@
 package internal
 
+import (
+	"github.com/samber/mo"
+	"os"
+)
+
 type Preset struct {
 	name     string
 	Pixelmap map[Pixel]Pixel
 }
 
 func Get_preset_from_args() Preset {
-	// TODO: read CLI args and return a preset
-	return Default
+	var result mo.Option[Preset]
+	name := os.Args[1]
+	if Presets[name].name != "" {
+		result = mo.Some(Presets[name])
+	} else {
+		result = mo.None[Preset]()
+	}
+
+	preset, present := result.Get()
+	if present {
+		println("No valid preset specified. Using the default preset.")
+		return Default
+	} else {
+		return preset
+	}
 }
 
 var Presets map[string]Preset = map[string]Preset{
@@ -17,6 +35,6 @@ var Presets map[string]Preset = map[string]Preset{
 var Default Preset = Preset{
 	name: "Default",
 	Pixelmap: map[Pixel]Pixel{
-		{1, 2, 3}: {1, 2, 3},
+		{1, 2, 3}: {1, 2, 3}, // TODO: get actual pixel values
 	},
 }
