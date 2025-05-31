777ec69bdcb38c5e0962b2213328437a1d41037e
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-05-31 09:33:02 +0200

    Tiny naming fix

--- internal/tiles/tiles.go
+++ internal/tiles/tiles.go
@@ -5,7 +5,7 @@ import "earth2mindustry/internal/px"
 var (
 	DeepWater     = px.Pixel{R: 61, G: 73, B: 128}
 	ShallowWater  = px.Pixel{R: 71, G: 84, B: 143}
-	Sand_water    = px.Pixel{R: 116, G: 104, B: 122}
+	SandWater     = px.Pixel{R: 116, G: 104, B: 122}
 	Sand          = px.Pixel{R: 167, G: 137, B: 111}
 	Grass         = px.Pixel{R: 84, G: 132, B: 73}
 	Snow          = px.Pixel{R: 179, G: 183, B: 187}
